c7a1132f96549d7c1b01b3fa8787a7b50b3254a5
JacktheRipper2187 <ulilf442@gmail.com>
2023-10-17 06:59:02 +0700

    kode sesudah di modifikasi

---
+++ PemilihanPercobaan222Modif.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class PemilihanPercobaan222Modif {
+    public static void main (String[] args) {
+        Scanner input22 = new Scanner(System.in);
+
+        System.out.print("Nilai uas     : ");
+        float uas = input22.nextFloat();
+        System.out.print("Nilai uts     : ");
+        float uts = input22.nextFloat();
+        System.out.print("Nilai kuis    : ");
+        float kuis = input22.nextFloat();
+        System.out.print("Nilai tugas   : ");
+        float tugas = input22.nextFloat();
+
+        float total = (uas * 0.4F) + (uts * 0.3F) + (kuis * 0.1F) + (tugas * 0.2F);
+        String message = total < 65 ? "Remidi" : "Tidak remidi";
+        if (total >= 80) {
+            System.out.println("Konversi nilai anda = A");
+        } else if (total >= 73) {
+            System.out.println("Konversi nilai anda = B+");
+        } else if (total >= 65) {
+            System.out.println("Konversi nilai anda = B");
+        } else if (total >= 60) {
+            System.out.println("Konversi nilai anda = C+");
+        } else if (total >= 50) {
+            System.out.println("Konversi nilai anda = C");
+        } else if (total >= 39) {
+            System.out.println("Konversi nilai anda = D");
+        } else {
+            System.out.println("Konversi nilai anda = E");
+        }
+
+        System.out.println("Nilai akhir = " + total + " sehingga " + message);
+ 
+        
+    }
+} 
